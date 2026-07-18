21cd3c3494675dcd77823611f0645f59408e40e1
acloudygirl <283527013@qq.com>
2026-07-18 15:55:35 +0800

    docker with chinese mirror url

--- modules/docker-setting.nix
+++ modules/docker-setting.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 {
-    virtualisation.docker.enable = true;
-    users.users.cloudygirl.extraGroups = [ "docker" ];
+    virtualisation.docker.enable = true;         #启用docker
+    users.users.cloudygirl.extraGroups = [ "docker" ];  #加入权限组
+    virtualisation.docker.autoPrune.enable = true;   #docker自动清理垃圾功能
+    virtualisation.docker.autoPrune.dates = "weekly"; #每周清理垃圾
 
+    virtualisation.docker.daemon.settings = { #启用国内镜像源
+        registry-mirrors = [
+        "https://docker.m.daocloud.io"
+        ];
+    };
 }
